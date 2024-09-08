d90e8e2fe6adea8a8578099ed8592b51426b6223
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-08 02:04:09 +0100

    commit

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Task from './Task'
+import useStore from '../store';
+import { shallow } from 'zustand/shallow';
 
 const Column = ({ state }) => {
+    const tasks = useStore((store) => 
+        store.tasks.filter((task) => task.state === state),
+        shallow
+    );
+
   return (
     <div className=' bg-gray-900 w-[30%] min-w-[250px] min-h-[20rem] rounded-md p-[0.5rem] text-white space-y-2'>
         <p>{state}</p>
-        <Task title={"Todo"} />
+        {tasks.map((task) => <Task title={task.title} key={task.title} />)}
     </div>
   )
 }
