c0bbfb76a4307c1c353f4456685977a8e1ec7b39
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-08 01:16:16 +0100

    commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 const Task = ({ title }) => {
-    const [status, setStatus] = useState("Planned")
+    const [status, setStatus] = useState("PLANNED")
 
   return (
     <div className=' bg-white rounded-md min-h-[5rem] text-black p-[0.5rem] flex flex-col justify-between'>
         {title}
         <div className=' flex justify-between'>
             <div></div>
-            <div className=' rounded-md p-[0.25rem] bg-gray-500 text-sm'>{status}</div>
+            <div className={`rounded-md p-[0.25rem] text-sm text-white ${status === "ONGOING" ? "bg-orange-600"
+            : status === "DONE" ? "bg-green-600" : "bg-gray-600"}`}>
+                {status}
+            </div>
         </div>
     </div>
   )
