ee49ee2e3a64235a890c7a77e7c112fafc5690ea
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-11 16:30:41 +0100

    commit

--- src/store.jsx
+++ src/store.jsx
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useStore = create((set) => {
     return {
         tasks: [{title: "TestTask", state: "ONGOING"}],
+        addTasks: (title, state) => 
+            set((store) => ({tasks: [...store.tasks, { title, state }]}))
     }
 })
 
