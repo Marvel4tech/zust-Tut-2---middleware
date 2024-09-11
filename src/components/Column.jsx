0f137ab5de89cf4614bb7456ed65a8c5bf32dfc1
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-11 21:28:08 +0100

    commit

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Task from './Task'
 import useStore from '../store';
 import { shallow } from 'zustand/shallow';
 
 const Column = ({ state }) => {
+    const [text, setText] = useState('')
+    const [open, setOpen] = useState(false)
+
     const tasks = useStore((store) => 
         store.tasks.filter((task) => task.state === state),
         shallow
@@ -15,11 +18,23 @@ const Column = ({ state }) => {
     <div className=' bg-gray-900 w-[30%] min-w-[250px] min-h-[20rem] rounded-md p-[0.5rem] text-white space-y-2'>
         <div className=' flex justify-between'>
             <p>{state}</p>
-            <button onClick={() => {addTask("At Home" + state, state)}} className=' bg-white px-4 py-1 text-black rounded-lg hover:bg-gray-500 hover:text-white'>
+            <button onClick={() => setOpen(true)} className=' bg-white px-4 py-1 text-black rounded-lg hover:bg-gray-500 hover:text-white'>
                 Add
             </button>
         </div>
         {tasks.map((task) => <Task title={task.title} key={task.title} />)}
+        {open && <div>
+            <div>
+                <input onChange={(e) => setText(e.target.value)} value={text} />
+                <button onClick={() => {
+                    addTask(text, state)
+                    setText('')
+                    setOpen(false)
+                }} >
+                    Submit
+                </button>
+            </div>
+        </div>}
     </div>
   )
 }
