d7d6fec9df87ec80892ebf01bc9a226dc512a755
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-09-11 16:48:39 +0100

    commit

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -9,11 +9,13 @@ const Column = ({ state }) => {
         shallow
     );
 
+    const addTask = useStore((store) => store.addTasks)
+
   return (
     <div className=' bg-gray-900 w-[30%] min-w-[250px] min-h-[20rem] rounded-md p-[0.5rem] text-white space-y-2'>
         <div className=' flex justify-between'>
             <p>{state}</p>
-            <button className=' bg-white px-4 py-1 text-black rounded-lg hover:bg-gray-500 hover:text-white'>
+            <button onClick={() => {addTask("At Home" + state, state)}} className=' bg-white px-4 py-1 text-black rounded-lg hover:bg-gray-500 hover:text-white'>
                 Add
             </button>
         </div>
--- src/store.jsx
+++ src/store.jsx
@@ -4,7 +4,7 @@ const useStore = create((set) => {
     return {
         tasks: [{title: "TestTask", state: "ONGOING"}],
         addTasks: (title, state) => 
-            set((store) => ({tasks: [...store.tasks, { title, state }]}))
+            set((store) => ({tasks: [...store.tasks, { title, state }]})),
     }
 })
 
